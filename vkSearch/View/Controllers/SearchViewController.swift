bed1a33d5bba6f777c281068e7cfe54145f87327
Alexander <kandallov@mac-mini-alexander.local>
2018-07-09 10:39:21 +0500

    Binding SearchViewController

--- vkSearch/View/Controllers/SearchViewController.swift
+++ vkSearch/View/Controllers/SearchViewController.swift
@@ -7,19 +7,49 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class SearchViewController: UIViewController {
+  
+  @IBOutlet weak var tableView: UITableView!
+  @IBOutlet weak var searchBar: UISearchBar!
+  
+  var viewModel: UsersViewModeling!
+  private let disposeBag = DisposeBag()
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    setupBindings()
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  private func setupBindings() {
+    searchBar.rx.text.orEmpty
+      .bind(to: viewModel.searchText)
+      .disposed(by: disposeBag)
+    
+    viewModel.cellModels
+      .bind(to: tableView.rx.items(cellIdentifier: "UserCell", cellType: UserCell.self)) { index, model, cell in
+        cell.viewModel = model
+      }.disposed(by: disposeBag)
+    
+    tableView.rx.contentOffset
+      .subscribe(onNext: { [unowned self] _ in
+        if self.searchBar.isFirstResponder {
+          self.searchBar.resignFirstResponder()
+        }
+      }).disposed(by: disposeBag)
+    
   }
+  
+}
 
-
+extension SearchViewController: UITableViewDelegate {
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+  }
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 70
+  }
 }
 
